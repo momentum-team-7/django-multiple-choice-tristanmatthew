81eba379c1c5db886b064776e109c5ffdc7dbcb9
Laura Quantrille <lquantrille@gmail.com>
2021-03-10 22:01:18 -0500

    security fix

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,7 @@ from .models import Profile, Snippet
 class SnippetForm(forms.ModelForm):
     class Meta:
         model = Snippet
-        fields = ['title','code','language', 'user']
+        fields = ['title','code','language']
 
 class ProfileForm(forms.ModelForm):
     class Meta:
